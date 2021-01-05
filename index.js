e50f000ab17331d8d939c174aa380c4bb319e7dd
Vencislav Kostadinov <vencislavkostadinov.bs@gmail.com>
2021-01-05 19:16:49 +0200

    Finished the project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Variables
 const toDoInput = document.querySelector('#todo-input');
 const liItem = document.getElementsByClassName('li');
+const icon = document.getElementsByClassName('trash-icon');
 
 // Adds new task when 'Add' button is clicked
 
@@ -26,10 +27,16 @@ document.querySelector('.btn-add').addEventListener('click', function () {
     newLiItem.appendChild(newEl);
     toDoInput.value = '';
 
-    // When list items are clicked adds red background color and line throught the item with '.finished-task' class. On second click removes the class from the item
+    // When list items are clicked adds background color and line throught the item with class '.finished-task'. On second click removes the class from the item.
     for (let i = 0; i < liItem.length; i++) {
       liItem[i].onclick = function () {
         liItem[i].classList.toggle('finished-task');
+      };
+    }
+    // When trash icon is clicked - list item on that row is removed
+    for (let i = 0; i < icon.length; i++) {
+      icon[i].onclick = function () {
+        liItem[i].style.display = 'none';
       };
     }
   }
